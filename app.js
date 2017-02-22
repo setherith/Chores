62a2af7e318105aa8832d46e7a4f8296e88b0a0c
Shane Pudner <setherith@hotmail.co.uk>
2017-02-22 16:50:13 +0000

    Working stub

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.engine('html', hbs.__express);
+
+app.set('views', __dirname + '/views');
+app.set
+
+app.use('/', function(req, res) {
+	res.render('test.html', {'message' : 'hello rendered world'});
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Server Running...on port " + (process.env.PORT || 3000));
+});
