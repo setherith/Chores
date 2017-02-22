6d7f78b262f041f506a7a59e99ed7388932785e6
Shane <setherith@hotmail.co.uk>
2017-02-22 19:15:00 +0000

    Form feedback added (and vs code files)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var hbs = require('hbs');
 var sql = require('mysql');
+var parser = require('body-parser');
 
 var app = express();
 
@@ -11,6 +12,12 @@ app.set('views', __dirname + '/views');
 
 hbs.registerPartials(__dirname + '/views/partials');
 
+app.use(parser.urlencoded({
+	extended: true
+}));
+
+app.use(parser.json());
+
 var conn = sql.createConnection({
 	host: "localhost",
 	user: "chores_user",
@@ -18,12 +25,19 @@ var conn = sql.createConnection({
 	database: "chores"
 });
 
-app.use('/', function(req, res) {
+app.get('/', function(req, res) {
 	conn.query('select * from tasks', function(err, rows) {
 		if(err) throw err;
 		res.render('test.html', {'message' : rows});
 	});
 	
+});
+
+
+
+app.post('/update', function(req, res) {
+	console.log(req.body);
+	res.redirect('/');
 });
 
 app.listen(process.env.PORT || 3000, function() {
