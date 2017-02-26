8b614f36ba1b9572b088b58d51b2e5e68db6d2e6
Shane Pudner <setherith@gmail.com>
2017-02-26 09:25:25 +0000

    Moved user view controllers to own file

--- controllers.js
+++ controllers.js
@@ -1,26 +1,8 @@
 module.exports = function(app, conn) {
-    
-    // display login form
-    app.get('/user/login', function(req, res) {
-        res.render('./user/login.html');
-    });
 
-    // process login request
-    app.post('/user/login', function(req, res) {
-        conn.query('select validate_user(\'' 
-                    + req.body.username + '\', \'' 
-                    + req.body.password + '\');', function(err, rows, fields) {
-            if (err) console.log(err);
-            if (rows[0][fields[0].name] == 0) {
-                // Not Valid
-                console.log('Failed login');
-            } else if(rows[0][fields[0].name] == 1) {
-                // Valid
-                console.log('User [' + req.body.username + '] logged on.');
-                res.render('./user/info.html');
-            }
-        });
-    });
+    //user controller
+    var userController = require('./views/user/userController.js');
+    userController(app, conn);
 
     // display the home page
     app.get('/', function(req, res) {
@@ -28,32 +10,6 @@ module.exports = function(app, conn) {
             if(err) console.log(err);
             res.render('index.html', {'model' : rows, 'fields' : fields});
         });
-    });
-
-    app.get('/user/add', function(req, res) {
-        res.render('./user/add.html');
-    });
-
-    app.post('/user/add', function(req, res) {
-        //Validation
-        var valid = false;
-
-        if(req.body.password != req.body.confirmpw) {
-            res.render('./user/add.html', { 
-                'err': 'Passwords don\'t match',
-                'username' : req.body.username,
-                'email' : req.body.email
-            });
-            valid = false;
-        }
-            
-        if(valid)
-            res.render('./user/info.html');
-    });
-
-    app.post('/update', function(req, res) {
-        console.log(req.body);
-        res.redirect('/');
     });
 
 };
