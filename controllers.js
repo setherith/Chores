ac5e99e674202c294b8c060a08564d56a55eaacb
Shane Pudner <setherith@gmail.com>
2017-03-04 11:36:00 +0000

    Added user admin

--- controllers.js
+++ controllers.js
@@ -12,6 +12,12 @@ module.exports = function(app, conn) {
         res.render('test.html', {'message' : "Hello, World!"});
     });
 
+    app.get('/admin/users', function(req, res) {
+        conn.query('select * from users', function(err, rows) {
+            res.render('./admin/users.html', {'users' : rows});
+        });
+    });
+
     // display the home page
     app.get('/', function(req, res) {
         conn.query('select t.*, u.username creator from tasks t, users u where t.creator = u.id;', function(err, rows) {
